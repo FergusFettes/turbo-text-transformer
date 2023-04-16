2ecbce5149c5d5abbe26ff386824e0b989b930a0
fergus <fergusfettes@gmail.com>
2023-04-16 21:58:27 +0100

    refactor again

--- ttt/__main__.py
+++ ttt/__main__.py
@@ -1,58 +1,12 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
-
 import click
-from dotenv import load_dotenv
 from langchain.llms import OpenAI
-# from langchain.text_splitter import CharacterTextSplitter
 from tttp.prompter import Prompter
 
-# from ttt.chunker import Chunker
-from ttt.config import arg2dict, check_config, get_encoding, model_tokens, save_config
+from ttt.config import Config
+from ttt.io import IO
 from ttt.tree import DummyTree, Tree
-
-load_dotenv()
-
-
-def check_chat(toggle, default, config):
-    if toggle:
-        config["chat"] = not config["chat"]
-        click.echo(f"Chat mode is now {'on' if config['chat'] else 'off'}.", err=True)
-
-    if default:
-        config["chat_file"] = default
-
-    if config["chat"] or config["chat_file"]:
-        save_config(config)
-
-    if config["chat"]:
-        click.echo(f"Chat mode is on. Using {config['chat_file']} as the chat history file.", err=True)
-
-
-def prepare_engine_params(params):
-    """Prepare options for the OpenAI API."""
-    params = {k: v for k, v in params.items() if v is not None}
-
-    params["max_tokens"] = model_tokens.get(params["model"], 2048)
-    if "number" in params:
-        params["n"] = params.pop("number")
-    return params
-
-
-def get_prompt(prompt, prompt_file):
-    """Get the prompt from stdin if it's not provided."""
-    if prompt_file:
-        prompt = Path(prompt_file).read_text().strip()
-        return prompt
-    if not prompt:
-        click.echo("Reading from stdin... (Ctrl-D to end)", err=True)
-        prompt = click.get_text_stream("stdin").read().strip()
-        click.echo("Generating...", err=True)
-        if not prompt:
-            click.echo("No prompt provided. Use the -p flag or pipe a prompt to stdin.", err=True, color="red")
-            raise click.Abort()
-    return prompt
 
 
 def simple_gen(tree):
@@ -62,10 +16,9 @@ def simple_gen(tree):
 
 
 def init(reinit, toggle_chat, default_chat, prompt, prompter, params):
-    # click.echo(params, err=True)
-    config = check_config(reinit)
-    check_chat(toggle_chat, default_chat, config)
-    params = prepare_engine_params(params)
+    config = Config.check_config(reinit)
+    Config.check_chat(toggle_chat, default_chat, config)
+    params = Config.prepare_engine_params(params)
 
     if config["chat"]:
         tree = Tree(config["chat_file"], params=params)
@@ -81,19 +34,8 @@ def init(reinit, toggle_chat, default_chat, prompt, prompter, params):
     return tree
 
 
-def handle_return(response, prompt, prompt_file):
-    if prompt_file:
-        with open(prompt_file, "a") as f:
-            f.write("\n".join(response))
-        return
-    sink = click.get_text_stream("stdout")
-    if prompt:
-        sink.write(prompt + "\n")
-    sink.write(response)
-
-
 def response_length(tree):
-    encoding = get_encoding(tree.params.get("model", "gpt-3.5-turbo"))
+    encoding = Config.get_encoding(tree.params.get("model", "gpt-3.5-turbo"))
     tree.params["max_tokens"] -= len(encoding.encode(tree.prompt))
 
 
@@ -125,10 +67,11 @@ def main(
     template_args,
     **params,
 ):
-    prompt = get_prompt(prompt, prompt_file)
-    prompter = Prompter.from_file(template_file, arg2dict(template_args)) if template_file else None
+    prompt = IO.get_prompt(prompt, prompt_file)
+    prompter = Prompter.from_file(template_file, Config.arg2dict(template_args)) if template_file else None
+
     tree = init(reinit, toggle_chat, default_chat, prompt, prompter, params)
     response_length(tree)
 
     response = tree.prompt if params["model"] == "test" else simple_gen(tree)
-    handle_return(response, prompt if echo_prompt else None, prompt_file if append else None)
+    IO.return_prompt(response, prompt if echo_prompt else None, prompt_file if append else None)
--- ttt/config.py
+++ ttt/config.py
@@ -6,6 +6,9 @@ import click
 import tiktoken
 import tttp
 import yaml
+from dotenv import load_dotenv
+
+load_dotenv()
 
 file_path = Path("/tmp/ttt/")
 file_path.mkdir(parents=True, exist_ok=True)
@@ -24,103 +27,125 @@ logging.basicConfig(
 )
 logger = logging.getLogger()
 
-config_dir = Path().home() / ".config/ttt"
-config_path = config_dir / "config.yaml"
 
-
-def load_config():
-    global config
-    if not config_path.exists():
-        config = {}
-        return
-    config = yaml.load(config_path.read_text(), Loader=yaml.Loader)
-    return config
-
-
-def save_config(config):
-    config_path.write_text(yaml.dump(config))
-
-
-def create_config():
-    config_dir.mkdir(parents=True, exist_ok=True)
-    config_path.write_text(yaml.dump(TURBO_TEXT_TRANSFORMER_DEFAULT_PARAMS))
-
-    # Find the templates, and make sure they are in the right place
-    tttp_dir = Path(tttp.__file__).parent
-    new_templates = tttp_dir.parent / "templates"
-    templates = config_dir / "templates"
-    templates.mkdir(parents=True, exist_ok=True)
-    for template in new_templates.glob("*.j2"):
-        if not (templates / template.name).exists():
-            (templates / template.name).write_text(template.read_text())
-
-
-def create_openai_config(api_key):
-    path = config_dir / "openai.yaml"
-
-    config = {
-        "engine_params": OPENAI_DEFAULT_PARAMS,
-        "api_key": api_key,
-        "backup_path": str(config.get("backup_path", "/tmp/ttt/")),
-        "models": [],
+class Config:
+    model_tokens = {
+        "gpt-4": 4096 - 8,
+        "gpt-3.5-turbo": 4096 - 8,
+        "text-davinci-003": 4096 - 8,
+        "text-davinci-002": 4096 - 8,
+        "code-davinci-002": 4096 - 8,
     }
-    path.write_text(yaml.dump(config))
+    TURBO_TEXT_TRANSFORMER_DEFAULT_PARAMS = {"format": "clean", "echo_prompt": False, "backup_path": "/tmp/ttt/"}
+    OPENAI_DEFAULT_PARAMS = {
+        "frequency_penalty": 0,
+        "logprobs": 1,
+        "max_tokens": 200,
+        "model": "gpt-3.5-turbo",
+        "n": 1,
+        "presence_penalty": 0,
+        "stop": None,
+        "temperature": 0.9,
+        "top_p": 1,
+    }
+    config_dir = Path().home() / ".config/ttt"
+    config_path = config_dir / "config.yaml"
 
-
-def check_config(reinit):
-    """Check that the config file exists."""
-    if not reinit and config_path.exists():
+    @staticmethod
+    def load_config():
+        global config
+        if not Config.config_path.exists():
+            config = {}
+            return
+        config = yaml.load(Config.config_path.read_text(), Loader=yaml.Loader)
         return config
 
-    click.echo("Config file not found. Creating one for you...", err=True, color="red")
-    create_config()
-    openai_api_key = click.prompt("OpenAI API Key", type=str)
-    if openai_api_key:
-        create_openai_config(openai_api_key)
+    @staticmethod
+    def save_config(config):
+        Config.config_path.write_text(yaml.dump(config))
 
-    return load_config()
+    @staticmethod
+    def create_config():
+        Config.config_dir.mkdir(parents=True, exist_ok=True)
+        Config.config_path.write_text(yaml.dump(Config.TURBO_TEXT_TRANSFORMER_DEFAULT_PARAMS))
 
+        # Find the templates, and make sure they are in the right place
+        tttp_dir = Path(tttp.__file__).parent
+        new_templates = tttp_dir.parent / "templates"
+        templates = Config.config_dir / "templates"
+        templates.mkdir(parents=True, exist_ok=True)
+        for template in new_templates.glob("*.j2"):
+            if not (templates / template.name).exists():
+                (templates / template.name).write_text(template.read_text())
 
-def get_encoding(model):
-    try:
-        encoding = tiktoken.encoding_for_model(model)
-    except (KeyError, ValueError):
-        encoding = tiktoken.get_encoding("gpt2")
-    return encoding
+    @staticmethod
+    def create_openai_config(api_key):
+        path = Config.config_dir / "openai.yaml"
 
+        config = {
+            "engine_params": Config.OPENAI_DEFAULT_PARAMS,
+            "api_key": api_key,
+            "backup_path": str(config.get("backup_path", "/tmp/ttt/")),
+            "models": [],
+        }
+        path.write_text(yaml.dump(config))
 
-TURBO_TEXT_TRANSFORMER_DEFAULT_PARAMS = {"format": "clean", "echo_prompt": False, "backup_path": "/tmp/ttt/"}
+    @staticmethod
+    def check_config(reinit):
+        """Check that the config file exists."""
+        if not reinit and Config.config_path.exists():
+            return config
 
-OPENAI_DEFAULT_PARAMS = {
-    "frequency_penalty": 0,
-    "logprobs": 1,
-    "max_tokens": 200,
-    "model": "gpt-3.5-turbo",
-    "n": 1,
-    "presence_penalty": 0,
-    "stop": None,
-    "temperature": 0.9,
-    "top_p": 1,
-}
+        click.echo("Config file not found. Creating one for you...", err=True, color="red")
+        Config.create_config()
+        openai_api_key = click.prompt("OpenAI API Key", type=str)
+        if openai_api_key:
+            Config.create_openai_config(openai_api_key)
 
+        return Config.load_config()
 
-load_config()
+    @staticmethod
+    def get_encoding(model):
+        try:
+            encoding = tiktoken.encoding_for_model(model)
+        except (KeyError, ValueError):
+            encoding = tiktoken.get_encoding("gpt2")
+        return encoding
 
-
-def arg2dict(args):
-    d = {}
-    if "=" not in args:
+    @staticmethod
+    def arg2dict(args):
+        d = {}
+        if "=" not in args:
+            return d
+        for arg in args.split(","):
+            k, v = arg.split("=")
+            d[k] = v
         return d
-    for arg in args.split(","):
-        k, v = arg.split("=")
-        d[k] = v
-    return d
+
+    @staticmethod
+    def prepare_engine_params(params):
+        """Prepare options for the OpenAI API."""
+        params = {k: v for k, v in params.items() if v is not None}
+
+        params["max_tokens"] = Config.model_tokens.get(params["model"], 2048)
+        if "number" in params:
+            params["n"] = params.pop("number")
+        return params
+
+    @staticmethod
+    def check_chat(toggle, default, config):
+        if toggle:
+            config["chat"] = not config["chat"]
+            click.echo(f"Chat mode is now {'on' if config['chat'] else 'off'}.", err=True)
+
+        if default:
+            config["chat_file"] = default
+
+        if config["chat"] or config["chat_file"]:
+            Config.save_config(config)
+
+        if config["chat"]:
+            click.echo(f"Chat mode is on. Using {config['chat_file']} as the chat history file.", err=True)
 
 
-model_tokens = {
-    "gpt-4": 4096 - 8,
-    "gpt-3.5-turbo": 4096 - 8,
-    "text-davinci-003": 4096 - 8,
-    "text-davinci-002": 4096 - 8,
-    "code-davinci-002": 4096 - 8,
-}
+Config.load_config()
